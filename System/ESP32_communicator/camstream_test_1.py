8f2c749e624b318e8e6c05b1f5e9e3e0a1a4be77
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-07 16:16:45 +0530

    🚑 By GRACE, now could able to stream.!!

--- System/ESP32_communicator/camstream_test_1.py
+++ System/ESP32_communicator/camstream_test_1.py
@@ -2,15 +2,28 @@ import websockets
 import asyncio
 import base64
 import urllib.request
+import numpy as np
+import cv2
 
 async def listen():
-    url = "ws://192.168.4.1/Camera"
+    url = "ws://192.168.81.165:81"
 
     async with websockets.connect(url) as ws:
         while True:
             msg = await ws.recv()
-            # print(type(msg))  # getting as 'byte' type -- indicates "binary data"
-            print(base64.b32decode(msg))
+            # print("Received message", msg)
+            npimg = np.array(bytearray(msg), dtype=np.uint8) # even try with msg.data
+            print(npimg)
+            img = cv2.imdecode(npimg, -1)
+            cv2.imshow("img", img)
+            cv2.waitKey(1)
+            # decoded = base64.b64decode(msg, validate=False)
+            # print("decoded msg: ", decoded)
+
+            # response = urllib.request.urlopen(msg)
+            # print(response)
+            # # print(type(msg))  # getting as 'byte' type -- indicates "binary data"
+            # print(base64.b32decode(msg))
 
 asyncio.get_event_loop().run_until_complete(listen())
 
