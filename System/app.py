0797d9a82c2c7f594c34a5934456ce481d906978
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-09 12:31:23 +0530

    ✨ Added navigation functionality via Flask

--- System/app.py
+++ System/app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
 
+"""  ************************************** Flask application ************************************** """
 app = Flask(__name__)
 
+""" ******************** Routes  """
 @app.route('/')
-def check():
-    return render_template('index.html')
+def home():
+    return render_template('index.html', nav_bar='home')
+
+@app.route('/manual_mode')
+def manual_mode():
+    return render_template('manualMode.htm', nav_bar='manual_mode')
+
+@app.route('/training_mode')
+def training_mode():
+    return render_template('trainingMode.htm', nav_bar='training_mode')
+
+@app.route('/autonomous_mode')
+def autonomous_mode():
+    return render_template('autonomousMode.htm', nav_bar='auto_mode')
 
 if __name__ == '__main__':
     app.run(debug=True)
