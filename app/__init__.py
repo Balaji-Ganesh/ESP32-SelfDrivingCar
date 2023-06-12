aba303abe24bdda93214959544d8ac2741d42590
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-12 12:36:30 +0530

    ⚗️ #1 Socket-io way. Worked, next failed again

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
-from .middleware.communication import get_cam_feed
+# from .middleware.communication import get_cam_feed
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
+    app.config['SECRET_KEY'] = 'secret!'
+    socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)        # to run websockets for this flask app. A new server runs for this.
     
     """ ----------- Register blueprints -----------"""
     from .web import web as main_blueprint  # for displaying web-pages
-    from .feed import feed                  # for handling camera and ultra-sonic feed
+    # from .feed import feed                  # for handling camera and ultra-sonic feed
 
     app.register_blueprint(main_blueprint, url_prefix='/')
-    app.register_blueprint(feed, url_prefix='/feed')
+    # app.register_blueprint(feed, url_prefix='/feed')
 
-    return app
+    return socketio, app
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from app import create_app
 
-app = create_app()
+socketio, app = create_app()
+
+#FIXME: later figure out the way to place this in some file like, `web` and `feed`
+
+# Handle specific events -- client uses `emit` for these
+@socketio.on('manual_mode') # alternative to socketio.on_event('manual_mode', handler=handle_manual_mode, namespace='/')
+def handle_manual_mode(data):
+    app.logger.info(data)
+    print("Received data: "+str(data))
+    # socketio.emit('acknowledgement', "You are connected")
+
+    
+# socketio.on_event('manual_mode', handler=handle_message, namespace='/')
+
+# handle pre-defined events -- client uses `send` for these
+@socketio.on('message')
+def handle_message(data):
+    print('client says: '+data)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
