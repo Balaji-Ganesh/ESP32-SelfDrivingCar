e61f5297cc68154ba837ec3976a8424a3f842b06
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-12 13:26:11 +0530

    ⚗️✨ #1 Able to send image from Flask to Web-app.

--- main.py
+++ main.py
@@ -4,20 +4,40 @@ socketio, app = create_app()
 
 #FIXME: later figure out the way to place this in some file like, `web` and `feed`
 
+def send_image():
+    with open('640x480.jpg', 'rb') as f:
+        image_data = f.read()
+    socketio.emit('img_data', {'image': image_data})
+
+# Event handlers for connection events
+@socketio.on('connect')
+def handle_connect():
+    print("Client connected successfully");
+    # send image data..
+    print("Server about to send the image")
+    send_image()
+    print("Server sent the image")
+
+@socketio.on('disconnect')
+def handle_connect():
+    print("Client connected successfully");
+
 # Handle specific events -- client uses `emit` for these
 @socketio.on('manual_mode') # alternative to socketio.on_event('manual_mode', handler=handle_manual_mode, namespace='/')
 def handle_manual_mode(data):
     app.logger.info(data)
     print("Received data: "+str(data))
     # socketio.emit('acknowledgement', "You are connected")
+    socketio.send('test')
 
     
 # socketio.on_event('manual_mode', handler=handle_message, namespace='/')
 
 # handle pre-defined events -- client uses `send` for these
-@socketio.on('message')
+@socketio.on('test')
 def handle_message(data):
-    print('client says: '+data)
+    print('client says: '+str(data))
+    socketio.send("server sending data")
 
 
 if __name__ == '__main__':
