6ba3d99fca9a8b507a878b0c6df32cb3652e8222
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-12 22:21:09 +0530

    ♻️  #1 Now could able to call socketio events even in other file

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
-# from .middleware.communication import get_cam_feed
+
+socketio: SocketIO = SocketIO
+
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
     app.config['SECRET_KEY'] = 'secret!'
-    socketio = SocketIO(app, cors_allowed_origins="*")#, logger=True, engineio_logger=True)        # to run websockets for this flask app. A new server runs for this.
-    
+
     """ ----------- Register blueprints -----------"""
     from .web import web as main_blueprint  # for displaying web-pages
     # from .feed import feed                  # for handling camera and ultra-sonic feed
@@ -15,8 +16,17 @@ def create_app():
     app.register_blueprint(main_blueprint, url_prefix='/')
     # app.register_blueprint(feed, url_prefix='/feed')
 
-    
-    return socketio, app
+    global socketio
+    # , logger=True, engineio_logger=True)        # to run websockets for this flask app. A new server runs for this.
+    socketio = SocketIO(app, cors_allowed_origins="*")
+
+    return app
+
 
 # Create the application..
-socketio, app = create_app()
+app = create_app()
+print("Application created")
+
+# Get the event handlers
+from .middleware.communication.web_communicator import handle_connect, handle_stream, handle_disconnect
+from .middleware.communication import get_cam_feed
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from app import socketio, app
 import cv2
 import base64
+from app import socketio, app
 
 # FIXME: later figure out the way to place this in some file like, `web` and `feed`
 # alternative to socketio.on_event('manual_mode', handler=handle_manual_mode, namespace='/')
@@ -12,6 +12,11 @@ def handle_manual_mode(data):
     print("Received data: "+str(data))
     # socketio.emit('acknowledgement', "You are connected")
     socketio.send('test')
+
+# Event handlers for socketio
+# @socketio.on('connect')
+# def handle_connect():
+#     print("Client connected successfully")
 
 
 if __name__ == '__main__':
