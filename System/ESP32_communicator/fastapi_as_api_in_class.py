ab1556a51f0c21e7e61bebf27f172f179f3e8d8e
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-15 15:31:09 +0530

    ⚗️ #1 Now could able control the feeds with ChatGPT's help.

--- System/ESP32_communicator/fastapi_as_api_in_class.py
+++ System/ESP32_communicator/fastapi_as_api_in_class.py
@@ -1,101 +1,109 @@
-from fastapi import Depends, FastAPI
-from fastapi_utils.cbv import cbv
-from fastapi_utils.inferring_router import InferringRouter
-
+import asyncio
+from fastapi import FastAPI, APIRouter
 import websockets
+
 import cv2
 import numpy as np
-import asyncio
 import logging
 
-app = FastAPI()
-router = InferringRouter()  # Step 1: Create a router
-
-@cbv(router)  # Step 2: Create and decorate a class to hold the endpoints
-class Foo:
-    # Attributes
-    # Step 3: Add dependencies as class attributes
-    def __init__(self):
-        esp32_ip:str = '192.168.134.165'                       # set it to the assigned IP address to ESP32 when connected to WiFi.
-        camera_port:int = 81
-        data_port:int = 82                     # configured ports for camera and data-transfer in ESP32.
+class ESP32Manager:
+    def __init__(self, esp32IP:str, cam_port:int=81, data_port:int=82):
+        
+        esp32_ip:str = esp32IP                       # set it to the assigned IP address to ESP32 when connected to WiFi.
+        camera_port:int = cam_port
+        data_port:int = data_port                     # configured ports for camera and data-transfer in ESP32.
         self.camera_ws_url:str = "ws://"+esp32_ip+":"+str(camera_port)    # url of camera websockets
         self.data_txrx_url:str = "ws://"+esp32_ip+":"+str(data_port)      # url of data transfer websockets
 
-        self.cam_ws: websockets.legacy.client.WebSocketClientProtocol
-        self.data_ws: websockets.legacy.client.WebSocketClientProtocol
+        self.cam_task = None
+        self.data_task = None
 
+        self.task = None
+        self.router = APIRouter()
 
-    # Helper functions
-    async def _establish_line(self):
-        try:
-            self.cam_ws = await websockets.connect(self.camera_ws_url)
-            self.data_ws = await websockets.connect(self.data_txrx_url)
-            print("Communication line established. ", type(self.cam_ws))
-            return 'Line established'
-        except Exception as e:
-            print("[EXCEPTION] Couldn't establish line. Error: ", e)
-            return 'Error in establishing line'
+        @self.router.get("/")
+        async def read_root():
+            return {"Hello": "World"}
+        
+        @self.router.get("/camera-feed/{function}")
+        async def camera_feed_handler(function:str):
+            if function == 'start':
+                if self.cam_task is None:
+                    self.cam_task = asyncio.create_task(self._camera_client())
+                    return {"message": "Camera task started."}
+                else:
+                    return {"message": "Camera task is already running."}
+            elif function == 'stop':
+                if self.cam_task is not None:
+                    self.cam_task.cancel()
+                    try:
+                        await self.cam_task
+                    except asyncio.CancelledError:
+                        pass
+                    self.cam_task = None
+                    cv2.destroyAllWindows()
+                    return {"message": "Camera task stopped."}
+                else:
+                    return {"message": "No camera task is currently running."}
 
+        @self.router.get("/collision-distance/{function}")
+        async def collision_dist_handler(function:str):
+            if function == 'start':
+                if self.data_task is None:
+                    self.data_task = asyncio.create_task(self._collision_dist_fetcher())
+                    return {"message": "Collision-data task started."}
+                else:
+                    return {"message": "Collision-data task is already running."}
+            elif function == 'stop':
+                if self.data_task is not None:
+                    self.data_task.cancel()
+                    try:
+                        await self.data_task
+                    except asyncio.CancelledError:
+                        pass
+                    self.data_task = None
+                    return {"message": "Collision-data task stopped."}
+                else:
+                    return {"message": "No collision-data task is currently running."}
+        
     async def _camera_client(self):
-        print("------------------------------------------- Getting camera feed")
         try:
             cam_ws = await websockets.connect(self.camera_ws_url)
-            print("Connection established. Test: ", type(cam_ws))
-            print("About to fetch frames")
             while True:
                 msg = await cam_ws.recv()
-                # print("Received message", msg)
                 npimg = np.array(bytearray(msg), dtype=np.uint8) # even try with msg.data
-                # print(npimg)
                 img = cv2.imdecode(npimg, -1)
                 cv2.imshow("img", img)
                 if cv2.waitKey(1) == 27:
                     print('EXITING')
                     cv2.destroyAllWindows()
-                    return '--- by from camerafeed'
+                    return 'Camera feed stopped by user'
         except Exception as e:
             logging.error("[EXCEPTION] Camera streaming interrupted. Error: ", e)
         finally:
             await cam_ws.close()
             logging.debug("Camera Websockets Connection closed successfully")
         return 'ERROR in fetching feed'
-            
-    async def _collision_dist_client(self):
-        print("------------------------------------------- Getting ultra-sonic feed")
-        try:
-            data_ws = await websockets.connect(self.data_txrx_url)
+    
+    async def _collision_dist_fetcher(self):
+        async with websockets.connect(self.data_txrx_url) as websocket:
             while True:
-                msg = await data_ws.recv()
-                print("collsion distance: ", msg)
-        except Exception as e:
-            logging.error("[_collision_dist_client] Error in fetching ultrasonic feed. Error: ",e)
-            return 'Error in fetching collsion feed'
-        finally:
-            await data_ws.close()
-            logging.debug("[_collision_dist_client] Camera Websockets Connection closed successfully")
-        
+                message = await websocket.recv()
+                print(f"Received message: {message}")
 
-    @router.get('/establish-line')
-    async def establish(self):
-        task = asyncio.create_task(self._establish_line())
-        return await task
+                # Process the message or perform any other task logic
 
-    @router.get('/camera-feed/')
-    async def get_camera_feed(self):
-        try:
-            task = asyncio.create_task(self._camera_client())
-            return await task
-        except KeyboardInterrupt:
-            return 'Camera data fetching interrupted'
+                # response = f"Processed: {message}"
+                # await websocket.send(response)
+                # print(f"Sent response: {response}")
 
-    @router.get('/collision-distance')
-    async def get_collision_distance(self):
-        try:
-            # print("-----------------check:  ", self.data_ws)
-            task = asyncio.create_task(self._collision_dist_client())
-            return await task
-        except KeyboardInterrupt:
-            return 'Collsion data fetching interrupted'
 
-app.include_router(router)
+app = FastAPI()
+obj = ESP32Manager(esp32IP='192.168.134.165' )
+
+app.include_router(obj.router)
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.2", port=8000, reload=True)
+# modified version of chatGPT's help.
