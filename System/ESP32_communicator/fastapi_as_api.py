bc066f5fcd0b8b309967c2a619b7ecbedee434bf
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-15 11:50:06 +0530

    ⚗️ #1 Now could able to get the feed with async calls

--- System/ESP32_communicator/fastapi_as_api.py
+++ System/ESP32_communicator/fastapi_as_api.py
@@ -6,7 +6,7 @@ import numpy as np
 
 app = FastAPI()
 
-esp32_ip = '192.168.135.165'                          # set it to the assigned IP address to ESP32 when connected to WiFi.
+esp32_ip = '192.168.134.165'                          # set it to the assigned IP address to ESP32 when connected to WiFi.
 camera_port, data_port = 81, 82                     # configured ports for camera and data-transfer in ESP32.
 camera_ws_url = "ws://"+esp32_ip+":"+str(camera_port)    # url of camera websockets
 data_txrx_url = "ws://"+esp32_ip+":"+str(data_port)      # url of data transfer websockets
@@ -26,6 +26,9 @@ async def establish_line():
 async def camera_client():
     print("------------------------------------------- Getting camera feed")
     try:
+        cam_ws = await websockets.connect(camera_ws_url)
+        print("Connection established")
+        print("About to fetch frames")
         while True:
             msg = await cam_ws.recv()
             # print("Received message", msg)
@@ -35,6 +38,7 @@ async def camera_client():
             cv2.imshow("img", img)
             if cv2.waitKey(1) == 27:
                 print('EXITING')
+                cv2.destroyAllWindows()
                 return '--- by from camerafeed'
     except Exception as e:
         print("[EXCEPTION] Camera streaming interrupted")
@@ -42,33 +46,29 @@ async def camera_client():
         
 async def collision_dist_client():
     print("------------------------------------------- Getting ultra-sonic feed")
+    data_ws = await websockets.connect(data_txrx_url)
     while True:
         msg = await data_ws.recv()
         print("collsion distance: ", msg)
     
 @app.get('/establish-line')
 async def establish():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_until_complete(establish_line())
-    except KeyboardInterrupt:
-        return 'Line establishment interrupted'
+    task = asyncio.create_task(establish_line())
+    return await task
 
-@app.get('/camera-feed')
+@app.get('/camera-feed/')
 async def get_camera_feed():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(camera_client())
+        task = asyncio.create_task(camera_client())
+        return await task
     except KeyboardInterrupt:
         return 'Camera data fetching interrupted'
 
 @app.get('/collision-distance')
 async def get_collision_distance():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(collision_dist_client())
+        print("-----------------check:  ", data_ws)
+        task = asyncio.create_task(collision_dist_client())
+        return await task
     except KeyboardInterrupt:
         return 'Collsion data fetching interrupted'
