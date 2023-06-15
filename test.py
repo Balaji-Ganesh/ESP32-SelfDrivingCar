80bbe2d33ebab12950a5c9fb753921754f26392a
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-15 16:38:28 +0530

    ⚰️ #1 Removed unused files

--- test.py
+++
@@ -1,9 +0,0 @@
-"""
-This is just a test script to test the functionality made.
-Deleted later.
-"""
-from web import get_cam_feed
-# from app.middleware.communication.esp32_communicator import get_cam_feed
-import asyncio
-
-asyncio.get_event_loop().run_until_complete(get_cam_feed())
