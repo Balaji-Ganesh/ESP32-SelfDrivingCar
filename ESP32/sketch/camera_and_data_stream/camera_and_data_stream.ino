e8f2ec952b1b1434a1753a108e5b513d277c76db
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-06-19 12:08:20 +0530

    ⚰️ Removed HTML page serving

--- ESP32/sketch/camera_and_data_stream/camera_and_data_stream.ino
+++ ESP32/sketch/camera_and_data_stream/camera_and_data_stream.ino
@@ -36,47 +36,6 @@ WiFiServer server(80);
 uint8_t cam_num, data_num;
 bool is_connected = false;
 
-String index_html = "<html>\n \
-<head>\n \
-<title> WebSockets Client</title>\n \
-<script src='http://code.jquery.com/jquery-1.9.1.min.js'></script>\n \
-</head>\n \
-<body>\n \
-<img id='live' src=''>\n \
-</body>\n \
-</html>\n \
-<script>\n \
-jQuery(function($){\n \
-if (!('WebSocket' in window)) {\n \
-alert('Your browser does not support web sockets');\n \
-}else{\n \
-setup();\n \
-}\n \
-function setup(){\n \
-var host = 'ws://server_ip:81';\n \
-var socket = new WebSocket(host);\n \
-socket.binaryType = 'arraybuffer';\n \
-if(socket){\n \
-socket.onopen = function(){\n \
-}\n \
-socket.onmessage = function(msg){\n \
-var bytes = new Uint8Array(msg.data);\n \
-var binary= '';\n \
-var len = bytes.byteLength;\n \
-for (var i = 0; i < len; i++) {\n \
-binary += String.fromCharCode(bytes[i])\n \
-}\n \
-var img = document.getElementById('live');\n \
-img.src = 'data:image/jpg;base64,'+window.btoa(binary);\n \
-}\n \
-socket.onclose = function(){\n \
-showServerResponse('The connection has been closed.');\n \
-}\n \
-}\n \
-}\n \
-});\n \
-</script>";
-
 /******************************************** Helpers **********************************************/
 /////// WebSocket Events...
 void camWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
@@ -234,17 +193,6 @@ void streamCollisionDistFeed(uint8_t num)
   //    handle the collision here itself. Just for the information purpose, send it to the system.
 }
 
-void http_resp()
-{
-  WiFiClient client = server.available();
-  if (client.connected() && client.available())
-  {
-    client.flush();
-    client.print(index_html);
-    client.stop();
-  }
-}
-
 /************************ Arduino Functions ************************/
 void setup()
 {
@@ -264,7 +212,6 @@ void setup()
 
   /***************************** Start Web server ************************************/
   Serial.println("[LOG] Attempting to start web server ...");
-  index_html.replace("server_ip", IP); // this will be removed later after experiments
   server.begin();
   Serial.println("[LOG] Web server started ...");
 
@@ -281,7 +228,6 @@ void setup()
 
 void loop()
 {
-  http_resp();
   // stream camera feed from ESP32 to System
   camWebSocket.loop();
   // send and receive data: ESP32 <--> System
